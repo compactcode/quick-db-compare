a730c29247e494da144867e71ec2c11b1a561c72
Shanon McQuay <shanonmcquay@gmail.com>
2011-09-06 21:45:36 +1000

    Alternating snaphots.

--- compare.rb
+++ compare.rb
@@ -2,11 +2,8 @@ require 'bundler/setup'
 
 Bundler.require
 
-DB = Sequel.connect(:adapter=>'postgres', :host=>'localhost', :database=>'db_name', :user=>'db_user', :password=>'db_pw')
-
-tables = DB.tables
-
-def snapshot(output_dir)
+def snapshot(tables, output_dir)
+  Dir.mkdir(output_dir)
   tables.each do |table|
     File.open("#{output_dir}/#{table.to_s}.txt", 'w') do |file|
       table = DB[table].order(:id).each do |record|
@@ -16,5 +13,14 @@ def snapshot(output_dir)
   end
 end
 
-snapshot("before")
+DB = Sequel.connect(:adapter=>'postgres', :host=>'localhost', :database=>'db_name', :user=>'db_user', :password=>'db_pass')
 
+tables = DB.tables - [:schema_migrations]
+
+if Dir.exist?("after")
+  FileUtils.rm_r("before")
+  FileUtils.rm_r("after")
+end
+
+snapshot(tables, "after")  if     Dir.exist?("before")
+snapshot(tables, "before") unless Dir.exist?("before")
